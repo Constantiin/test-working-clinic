d3e452487154d19a75848d3ef970a7a7d3706b39
Cos <constantiin@bk.ru>
2021-10-31 22:25:43 +0300

    work process

---
+++ js/main.js
@@ -0,0 +1,5 @@
+if (window.innerWidth >= 1140) {
+    document.querySelector('.main__container').classList.remove('container')
+    document.querySelector('.main__content').classList.add('main__content_container')
+    console.log('Hey')
+}
