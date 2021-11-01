d8506fc821ba9f4f73aba0f469a850c596bb7aa9
Cos <constantiin@bk.ru>
2021-11-01 11:58:56 +0300

    fix scroll

--- js/main.js
+++ js/main.js
@@ -24,5 +24,4 @@ new Swiper(".swiper", {
 if (window.innerWidth >= 1140) {
     document.querySelector('.main__container').classList.remove('container')
     document.querySelector('.main__content').classList.add('main__content_container')
-    console.log('Hey')
 }
