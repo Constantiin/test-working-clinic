05e17bc44b03e68cc30054baab03db99f7e850a7
Cos <constantiin@bk.ru>
2021-11-01 11:22:26 +0300

    fix, add slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+new Swiper(".swiper", {
+    navigation: {
+        nextEl: ".main__arrow_right",
+        prevEl: ".main__arrow_left",
+        disabledClass: "main__arrow_disable"
+    },
+    // breakpoints: {
+    //     480: {
+    //         grid: {
+    //             fill: "row",
+    //             rows: 2
+    //         }
+    //     },
+    //     768: {
+    //         slidesPerView: 2,
+    //         grid: {
+    //             fill: "row",
+    //             rows: 2
+    //         }
+    //     }
+    // }
+});
+
 if (window.innerWidth >= 1140) {
     document.querySelector('.main__container').classList.remove('container')
     document.querySelector('.main__content').classList.add('main__content_container')
