9c90cf9f338ef453f053a1cb425694c57de640e1
Cos <constantiin@bk.ru>
2021-11-02 09:46:19 +0300

    add meta description

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Официальный сайт многопрофильной клиники для детей и взрослых в городе Ростове-на-Дону">
     <title>Многопрофильная клиника для детей и взрослых</title>
 
     <link rel="stylesheet" href="css/index.css">
